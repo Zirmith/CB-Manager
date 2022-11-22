f3fa2dfcba40c3bc2fe59dc1437028a460833d7a
_Auth <f2v74yt@gmail.com>
2022-11-22 16:39:53 -0600

    Update example_bot#2.lua

--- example_bot#2.lua
+++ example_bot#2.lua
@@ -1,4 +1,4 @@
-local CB_Manager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Zirmith/CB-Manager/main/require.lua"))()
+local CB_Manager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Zirmith/CB-Manager/main/require.lua"))() -- This is a loadstring silly .,.
 
 local config = CB_Manager:Configuration({
     ['prefix'] = "?",
@@ -7,14 +7,13 @@ local config = CB_Manager:Configuration({
     ['admins'] = {"User","Names","Here"},
     ['mods'] = {"User","Names","Here"},
     ['config_name'] = "Save#1"
-})
+}) -- This is the bot config, that will be used / saved
 
-local client = CB_Manager:init(config) -- This is used to download plugins
+local client = CB_Manager:init(config) -- this is to create the bot
 
-local example_plugin = client:getPlugin('example')
+local example_plugin = client:getPlugin('example') -- This is used to download plugins
 
 client:usePlugin(example_plugin, {
   ['args1'] = "",
   ['args2'] = ""
-})
-
+}) -- this is if you want to use a plugin , max args are currently [2]
