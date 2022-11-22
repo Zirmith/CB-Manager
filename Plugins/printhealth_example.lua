a4de18863bc02776017d4547d45e67d154c01e17
_Auth <f2v74yt@gmail.com>
2022-11-22 16:46:43 -0600

    Update printhealth_example.lua

--- Plugins/printhealth_example.lua
+++ Plugins/printhealth_example.lua
@@ -2,7 +2,7 @@ return {
     ["PluginName"] = "Health_Example",
     ["PluginDescription"] = "made by example.com",
     ["Commands"] = function(f1,f2)
-        local client = CB_Manager['Client']
+        local client = CB_Manager['Client'].Character
         local hum = client:FindFirstChild('Humanoid')
         print(hum.Health)
     end
