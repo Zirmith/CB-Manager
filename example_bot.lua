3813b88087d926822dbef72441a43b5f2d8165d4
_Auth <f2v74yt@gmail.com>
2022-11-22 08:33:04 -0600

    Update example_bot.lua

--- example_bot.lua
+++ example_bot.lua
@@ -1,1 +1,17 @@
-local CB_MANAGER = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Zirmith/Util-Tools/main/source.lua"))()
+local CB_MANAGER = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Zirmith/CB-Manager/main/create.lua"))()
+
+local config = CB_MANAGER:Configuration({
+    ['prefix'] = "?",
+    ['bot'] = "Server",
+    ['owner'] = "Your_User",
+    ['admins'] = {"User","Names","Here"},
+    ['mods'] = {"User","Names","Here"},
+    ['config_name'] = "Save#1"
+})
+
+CB_Manager:downloadPlugin('start') -- This is used to download plugins
+
+CB_Manager:runPlugin('start', {
+  ['args1'] = "",
+  ['args2'] = ""
+}) -- This is used to run bot plugins downloaded
