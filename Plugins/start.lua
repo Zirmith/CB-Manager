192c1103b262ccda12f870184ded24bbbf9631c8
_Auth <f2v74yt@gmail.com>
2022-11-22 08:36:11 -0600

    Update start.lua

--- Plugins/start.lua
+++ Plugins/start.lua
@@ -1,7 +1,8 @@
 return {
     ["PluginName"] = "DoPlugins",
     ["PluginDescription"] = "made by ps",
-    ["Commands"] = function()
+    ["Commands"] = function(f1,f2)
         print('Hello world')
+        warn(f1,f2)
     end
 }
