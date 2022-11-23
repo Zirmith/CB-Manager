4daeea8f63d6493be9453bae4f61c28349fd731d
_Auth <f2v74yt@gmail.com>
2022-11-22 22:11:50 -0600

    Update and rename owoifyv2.lua to owoifyv3.lua

--- Plugins/owoifyv3.lua
+++ Plugins/owoifyv3.lua
@@ -1,23 +1,22 @@
 
 return {
-            ["PluginName"] = "owoify",
+            ["PluginName"] = "owoifyv3",
             ["PluginDescription"] = "made by example.com",
             ["Commands"] = function(f1)
-                
-
-
-                local response = request(
+            local ez = f1
+			local response = request(
 			{
 				Url = "https://uwuaas.herokuapp.com/api",  -- This website helps debug HTTP requests
 				Method = "POST",
 				Headers = {
 					["Content-Type"] = "application/json"  -- When sending JSON, set this!
 				},
-				Body = game:GetService("HttpService"):JSONEncode({text = ""..f1})
+				Body = game:GetService("HttpService"):JSONEncode({text = ""..ez})
 			}
 		)
 
-        local uwuified
+        print(ez)
+		local uwuified
 
 		for i,v in pairs(response) do
 			if type(v) == "string" and v:find("text") then
@@ -25,14 +24,10 @@ return {
 			local f = g:gsub("}","")
 			local z = f:gsub([["]],"")
 			uwuified = z
-            warn(uwuified)
+			print(uwuified)
 			end
 		end
-
-        local A_1 = uwuified
-        local A_2 = 'All'
-
-        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(A_1, A_2)
-     end
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(uwuified,"All")
+            end
 }
             
