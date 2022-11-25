2ce6d1c818c0629f5eaced68b1b928ba9da821d4
_Auth <f2v74yt@gmail.com>
2022-11-25 09:45:53 -0600

    Update Health.lua

--- Plugins/Health.lua
+++ Plugins/Health.lua
@@ -1,9 +1,25 @@
+
 return {
-    ["PluginName"] = "Health_Example",
+    ["PluginName"] = "Health Notification v2",
     ["PluginDescription"] = "made by example.com",
     ["Commands"] = function(f1,f2)
+        local Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterNotifications.lua"))()
         local client = CB_Manager['Client'].Character
         local hum = client:FindFirstChild('Humanoid')
-        return(hum.Health)                    
+
+        Notification.Notify("Character Info | Health", hum.Health , "rbxasset://textures/ui/GuiImagePlaceholder.png", {
+            Duration = 2,       
+            Main = {
+                Rounding = true,
+            }
+        });
+        
+        Notification.WallNotification("Character Info | UserId", client.UserId, {
+                Duration = 3,
+        
+                TitleSettings = {
+                    Enabled = false
+                }
+            });
     end
 }
